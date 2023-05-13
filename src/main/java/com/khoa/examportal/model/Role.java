bf868b68d2a3bb22efb0a3bf0b9ef0818d87f4ef
DangKhoa97102 <dangkhoa97.102@gmail.com>
2023-05-13 14:35:05 +0700

    Create User Role Entity

---
+++ src/main/java/com/khoa/examportal/model/Role.java
@@ -0,0 +1,26 @@
+package com.khoa.examportal.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter @Setter
+@AllArgsConstructor @NoArgsConstructor
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long roleId;
+    private String roleName;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "role")
+    private Set<UserRole> userRoles = new HashSet<>();
+
+}
---
+++ src/main/java/com/khoa/examportal/model/User.java
@@ -0,0 +1,36 @@
+package com.khoa.examportal.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter @Setter
+@AllArgsConstructor @NoArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String phone;
+    private String profile;
+
+    //user many roles
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "user")
+    @JsonIgnore
+    private Set<UserRole> userRoles = new HashSet<>();
+
+    private boolean enabled = true;
+}
